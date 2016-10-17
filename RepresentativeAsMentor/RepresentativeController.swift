59a19666888d45279c74816f94d97c93dc042098
Jeff Norton <jeff12ntn@gmail.com>
2016-10-17 14:45:32 -0600

    RepController done

---
+++ RepresentativeAsMentor/RepresentativeController.swift
@@ -0,0 +1,76 @@
+//
+//  RepresentativeController.swift
+//  RepresentativeAsMentor
+//
+//  Created by Jeff Norton on 10/17/16.
+//  Copyright © 2016 JeffCryst. All rights reserved.
+//
+
+import Foundation
+
+class RepresentativeController {
+    
+    //==================================================
+    // MARK: - Properties
+    //==================================================
+    
+    static let baseURL = URL(string: "http://whoismyrepresentative.com/getall_reps_bystate.php")
+    private static let keyState = "state"
+    private static let keyResult = "result"
+    
+    //==================================================
+    // MARK: - Methods
+    //==================================================
+    
+    static func searchRepresentatives(forState state: String, completion: @escaping (_ representatives: [Representative]?) -> Void) {
+        
+        guard let url = self.baseURL else {
+            NSLog("Error: Invalid URL")
+            completion(nil)
+            return
+        }
+        
+        let urlParameters = [keyState : state]
+        
+        NetworkController.performRequest(for: url, httpMethod: .Get, urlParameters: urlParameters) { (data, error) in
+            
+            guard let data = data,
+                let responseDataString = String(data: data, encoding: .utf8) else {
+                    
+                    NSLog("No data returned from network request.")
+                    completion(nil)
+                    return
+            }
+            
+            guard let responseDictionary = (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)) as? [String : AnyObject],
+                let repDictionary = responseDictionary[keyResult] as? [[String : Any]] else {
+                    
+                    NSLog("Unable to serialize JSON. \nResponse: \(responseDataString)")
+                    completion(nil)
+                    return
+            }
+            
+            let representatives = repDictionary.flatMap({ Representative(dictionary: $0) })
+            completion(representatives)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
