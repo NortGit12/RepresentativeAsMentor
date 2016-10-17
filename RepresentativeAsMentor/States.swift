a926fbc867ac5e42076f12dbd36b52d8d0f162ef
Jeff Norton <jeff12ntn@gmail.com>
2016-10-17 15:10:29 -0600

    StateListViewController done

---
+++ RepresentativeAsMentor/States.swift
@@ -0,0 +1,13 @@
+//
+//  States.swift
+//  RepresentativeAsMentor
+//
+//  Created by Jeff Norton on 10/17/16.
+//  Copyright © 2016 JeffCryst. All rights reserved.
+//
+
+import Foundation
+
+enum States {
+    static var all = ["AK", "AL", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+}
